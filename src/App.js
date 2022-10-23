26e849b18fc873e015a773698f19f08f992eca4f
Richard Kim <trrichard16@yahoo.com>
2022-10-23 00:58:05 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -63,16 +63,13 @@ function App() {
     <h2>
       <AccordionButton>
         <Box flex='1' textAlign='left'>
-          Section 1 title
+          #1 Most Popular Reddit Topic
         </Box>
         <AccordionIcon />
       </AccordionButton>
     </h2>
     <AccordionPanel pb={4}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.
+      Insert body text here.
     </AccordionPanel>
   </AccordionItem>
 
@@ -80,16 +77,13 @@ function App() {
     <h2>
       <AccordionButton>
         <Box flex='1' textAlign='left'>
-          Section 2 title
+          #2 Most Popular Reddit Topic
         </Box>
         <AccordionIcon />
       </AccordionButton>
     </h2>
     <AccordionPanel pb={4}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.
+      Insert body here.
     </AccordionPanel>
   </AccordionItem>
 </Accordion>
