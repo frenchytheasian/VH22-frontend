3ca492db29613c8e8d243aa41978e8f43cdeef4a
Richard Kim <trrichard16@yahoo.com>
2022-10-23 00:45:18 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 import { Input, InputGroup, InputRightElement, Button, CircularProgress } from "@chakra-ui/react";
 import Data from "./Data";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+} from '@chakra-ui/react'
 
 function App() {
   const [submitted, setSubmitted] = useState(false);
@@ -50,6 +58,41 @@ function App() {
             GO
           </Button>} onClick={handleSubmit} />
         </InputGroup>
+        <Accordion>
+  <AccordionItem>
+    <h2>
+      <AccordionButton>
+        <Box flex='1' textAlign='left'>
+          Section 1 title
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel pb={4}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </AccordionPanel>
+  </AccordionItem>
+
+  <AccordionItem>
+    <h2>
+      <AccordionButton>
+        <Box flex='1' textAlign='left'>
+          Section 2 title
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel pb={4}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </AccordionPanel>
+  </AccordionItem>
+</Accordion>
       </div>
       {submitted && loaded && <Data data={sentiment} />}
     </>
